e9e257ea013bbaa6dd4dada76b6c5a301bc4acaf
jaanikahaki <146062060+jaanikahaki@users.noreply.github.com>
2024-01-29 15:28:24 +0200

    Add files via upload

---
+++ lumememm.py
@@ -0,0 +1,43 @@
+import pygame
+import sys
+
+pygame.init()
+
+# Akna suurus
+screen_size = (300, 300)
+
+# Ekraani loomine
+screen = pygame.display.set_mode(screen_size)
+pygame.display.set_caption("Lumememm - Jaanika Haki")
+
+
+# Peamine mängutsükkel
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Ekraani värv
+    screen.fill((0, 0, 0))
+    
+    # Ristküliku ja ringide suurused ja värvid
+    pygame.draw.rect(screen, [129, 126, 127], [100, 10, 100, 270], 2)
+    #rect_color = (140, 140, 140)
+    
+    # Ristkülik (vaid ümbritsev joon)
+    #rect = pygame.Rect((screen_size[0] - rect_size[0]) // 2, (screen_size[1] - rect_size[1]) // 2, *rect_size)
+    #pygame.draw.rect(screen, rect_color, rect, 2)  # 1 tähendab joone laiust
+
+    # Ringid (täidetud)
+    pygame.draw.circle(screen, [255, 0, 0], [150, 60], 40)
+    pygame.draw.circle(screen, [255, 255, 0], [150, 145], 40)
+    pygame.draw.circle(screen, [0, 255, 0], [150, 230], 40)
+
+    # Ekraani värskendamine
+    pygame.display.flip()
+
+# Pygame sulgemine
+pygame.quit()
+sys.exit()
+
