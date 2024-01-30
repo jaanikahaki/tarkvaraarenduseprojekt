254ada22a122598112391d5432d1ea125c65459e
jaanikahaki <146062060+jaanikahaki@users.noreply.github.com>
2024-01-30 15:57:25 +0200

    Update foor.py

--- foor.py
+++ foor.py
@@ -1,43 +1,37 @@
-import pygame
-import sys
+import pygame   # impordime pygame mooduli
+import sys      # impordime süsteemimooduli
 
-pygame.init()
+pygame.init()   # initsialiseerime Pygame'i
 
 # Akna suurus
-screen_size = (300, 300)
+screen_size = (300, 300)    # määrame tehtava akna suuruse - 300 x 300 pixlit
 
 # Ekraani loomine
-screen = pygame.display.set_mode(screen_size)
-pygame.display.set_caption("Foor - Jaanika Haki")
+screen = pygame.display.set_mode(screen_size)   # loome Pygame mooduli abil akna, millele on eelnevalt määratud ka suurus
+pygame.display.set_caption("Foor - Jaanika Haki")  # määrame Pygame aknale nimetuse
 
 
 # Peamine mängutsükkel
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+running = True    # muutuja running loomine
+while running:    # algab lõpmatu tsükkel, mis kestab kuni running = True
+    for event in pygame.event.get():   # 
+        if event.type == pygame.QUIT:  # kui kasutaja sulgeb Pygame abil loodud akna...
+            running = False   # siis Pygame katkestab akna tsükli
 
     # Ekraani värv
-    screen.fill((0, 0, 0))
+    screen.fill((0, 0, 0))   # määrab akna taustavärvi - must; rgb kood
     
-    # Ristküliku ja ringide suurused ja värvid
-    pygame.draw.rect(screen, [129, 126, 127], [100, 10, 100, 270], 2)
-    #rect_color = (140, 140, 140)
-    
-    # Ristkülik (vaid ümbritsev joon)
-    #rect = pygame.Rect((screen_size[0] - rect_size[0]) // 2, (screen_size[1] - rect_size[1]) // 2, *rect_size)
-    #pygame.draw.rect(screen, rect_color, rect, 2)  # 1 tähendab joone laiust
+    # Ristkülik
+    pygame.draw.rect(screen, [129, 126, 127], [100, 10, 100, 270], 2) #määrab fooritulesid ümbritseva ristküliku (ainult ümbritsev joon) värvi (hall), suuruse ja asukoha ning joone paksuse (2 pixlit)
 
     # Ringid (täidetud)
-    pygame.draw.circle(screen, [255, 0, 0], [150, 60], 40)
-    pygame.draw.circle(screen, [255, 255, 0], [150, 145], 40)
-    pygame.draw.circle(screen, [0, 255, 0], [150, 230], 40)
+    pygame.draw.circle(screen, [255, 0, 0], [150, 60], 40)  # foori esimene ring (punane, asukoht, suurus)
+    pygame.draw.circle(screen, [255, 255, 0], [150, 145], 40)  # foori teine ring (kollane, asukoht, suurus)
+    pygame.draw.circle(screen, [0, 255, 0], [150, 230], 40)   # foori esimene ring (roheline, asukoht, suurus)
 
     # Ekraani värskendamine
-    pygame.display.flip()
+    pygame.display.flip()  # Pygame ekraanivärskenduse funktsioon, mis toob nähtavale kõik eelnevalt loodud graafilised lahendused (foori)
 
 # Pygame sulgemine
-pygame.quit()
-sys.exit()
-
+pygame.quit() # lõpetab Pygame mooduli toimimise selle akna jaoks, kui aken on suletud, et vabastada arvuti ressursid
+sys.exit()    # lõpetab süsteemimooduli toimimise selle...
